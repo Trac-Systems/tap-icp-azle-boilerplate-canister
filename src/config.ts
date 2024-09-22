b37b2a4286e5a0f19bbd056e78135130de4e8778
TracSystems <markus.bopp@trac.network>
2024-09-22 23:15:52 +0200

    + initial revision

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,10 @@
  */
 
 export const network = { regtest : null }; // options: mainnet, testnet, regtest
+// must be an https url a tap reader's rest endpoint. On Bitcoin mainnet, the endpoint must be located on a domain with ipv6 SSL support
+// for local replicas of ICP, it is recommended to use a tap reader on the web with SSL cert. instead of local instance.
 export const API_HOST = '';
+// the mint tick can be any tap token mint as of this canister, but it should be a ticker that practically can never mint out.
 export const MINT_TICK = 'tapswap-v2';
 export const START_BLOCK = 3522;
 export const TOKEN_AUTH_INSCRIPTION = '';
